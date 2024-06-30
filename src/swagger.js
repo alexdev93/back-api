5dcb4eca932ca7d9030f77a16bde5c9e74c991d7
alemayehu.dev <alemayehu.dev@gmail.com>
2024-06-30 05:22:44 -0700

    add contact info

--- src/swagger.js
+++ src/swagger.js
@@ -3,13 +3,20 @@ const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './swagger_output.json';
 const endpointsFiles = ['./index.js']; // Path to your Express route files
 
+const { SERVER_URL } = process.env
+
 const doc = {
   info: {
     title: 'Megazin Server API', // Title of the API
     description: 'API documentation for News Service', // Description of the API
     version: '1.0.0', // Version of the API
+    contact: {
+      name: 'Alemayehu Mekonen', // Your name
+      email: 'alemayehu.dev@gmail.com', // Your email address
+      url: 'https://alemayehu.glitch.me', // Your website or GitHub profile
+    },
   },
-  host: 'localhost:9090', // Replace with your server's hostname and port
+  host: SERVER_URL, // Replace with your server's hostname and port
   basePath: '/', // Optional: Base path of the API (e.g., /v1)
   schemes: ['http'], // Specify the protocol used by your API (http, https)
 };
